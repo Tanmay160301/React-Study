d39cc7d440bad1b424b82ed6884f2b76c98ef0cb
Tanmay160301 <bhurketanmay@gmail.com>
2023-12-26 09:57:28 +0530

    26-11 changes

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -1,3 +1,34 @@
+/* 
+Basic expectation from this code:
+
+Props is responsible for making the components reusable
+
+Basic understanding of props and how it is responsible for passing values or properties from App.jsx to
+individual  components
+
+practically saw how Tailwindcss can be set up in react
+
+practically saw how props are passed 
+practically saw how props are handled
+practically saw how default values in props are handled
+
+Its a best practice to create a folder namely Components and create your components inside it and 
+then import those in app.jsx
+
+Some imp points from sir:
+
+props initially gives an empty object which is also linked with one of the prototype object
+
+props madhe key value aapn string directly pass karu shaktoy but when it comes to passing arrays or objects
+ or functions or variables we have to pass the reference of it
+
+Its a good practice ki props chya thikane object destructuring karne
+
+*/
+
+
+
+
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
